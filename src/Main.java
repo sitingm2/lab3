d7062dc545424e3ecc0e4188c5761eef17a46c16
Tina <sitingm2@illinois.edu>
2018-02-07 18:57:32 -0600

    Lab3 initialization done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,30 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static int numberCount (final String text) {
+        int count = urlToString(text).length();
+        return count;
+    }
 
     public static void main(String[] args) {
         System.out.println("Here");
